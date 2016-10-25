fcb7b49ba52604df26cb37e7af3384a418bb1853
Dshy <KortSkg@KORTSKG-PC>
2016-10-25 11:34:21 +0200

    Case 6 virker

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -15,21 +15,21 @@ namespace Sandbox
             aListOfInt.Add(9);
 
             // Case 1
-            // Console.WriteLine("Case 1: Element with index 1 is {0}", aListOfInt[1]);
-
+            Console.WriteLine("Case 1: Element with index 1 is {0}", aListOfInt[1]);
+            // 0 = 12
             // Case 2
-            // Console.WriteLine("Case 2: List contains {0} elements", aListOfInt.Count);
- 
+            Console.WriteLine("Case 2: List contains {0} elements", aListOfInt.Count);
+
             aListOfInt.Add(5);
             aListOfInt.Add(22);
 
             // Case 3
-            // Console.WriteLine("Case 3: Element with index 3 is {0}", aListOfInt[3]);
+            Console.WriteLine("Case 3: Element with index 3 is {0}", aListOfInt[3]);
 
             aListOfInt.RemoveAt(0);
 
             // Case 4
-            // Console.WriteLine("Case 4: Element with index 3 is {0}", aListOfInt[3]);
+            Console.WriteLine("Case 4: Element with index 3 is {0}", aListOfInt[3]);
 
 
 
@@ -43,19 +43,27 @@ namespace Sandbox
 
             // Case 5: Make some code that prints out 
             // all the elements in the list
-
+            Console.WriteLine("Case 5: The list contains {0} numbers", aListOfInt.Count);
 
 
             // Case 6: Make some code that finds the 
             // sum of the elements in the list, and prints the result
-
-
+            int Sum = 0;
+            foreach (int totalSum in aListOfInt)
+            {
+                Sum = totalSum + Sum;
+                Console.WriteLine(Sum);
+            }
 
             // Case 7: Make some code that finds the 
             // average of the elements in the list, and prints the result
             // Tip: Think about how average is defined; then
             // you might reuse something from Case 6
-
+            int average = 0;
+            foreach (int totalSum in aListOfInt)
+            {
+                
+            }
 
 
             // [DIFFICULT]
